adada59d0a5bc24a4148d4a1edde876f99afcdbb
AndresParraGO <andresparragn@gmail.com>
2020-04-29 11:43:06 -0500

    Add file index.html in src/

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,19 @@
 const {dest, src, watch, parallel} = require('gulp'),
        sass = require('gulp-sass')
        
-       
+    
+function html() {
+  return src('./src/index.html')
+        .pipe(dest('./public'))
+}
+
+
 function styles() {
   return src('./src/scss/main.scss')
-          .pipe(sass({
-            outputStyle: 'compressed'
-          }))
-          .pipe(dest('./public/'))
+        .pipe(sass({
+          outputStyle: 'compressed'
+        }))
+        .pipe(dest('./public/'))
 }
 
 
@@ -18,10 +24,12 @@ function assets() {
 }
 
 
-
+exports.html = html
 exports.styles = styles
 exports.assets = assets
-exports.build = parallel(assets, styles)
+exports.build = parallel(assets, html, styles)
+
 exports.default = () => {
+  watch('./src/index.html', html)
   watch('./src/scss**/*.scss', styles)
 }
